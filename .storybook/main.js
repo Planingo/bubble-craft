786b4fdddcc23e5043171bf9ad2ba63b49d79b78
Marina Blin <m4rina.blin@gmail.com>
2021-08-21 21:06:56 +0200

    :sparkles: Ajout des couleurs

--- .storybook/main.js
+++ .storybook/main.js
@@ -12,4 +12,25 @@ module.exports = {
     '@storybook/addon-a11y',
     '@storybook/addon-essentials'
   ],
+  webpackFinal: async (config, { configType }) => {
+    const cssModel = config.module.rules.find(i => i.test.toString() === "/\\.css$/")
+    let lessRule = {
+      test: /\.less$/,
+      sideEffects: true,
+      use: [
+        ...cssModel.use,
+        {
+          loader: 'less-loader',
+          options: {
+            lessOptions: {
+              javascriptEnabled: true
+            }
+          }
+        }
+      ],
+      
+    }
+    config.module.rules.push(lessRule)
+    return config
+  },
 }
