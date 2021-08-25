62653d1f6478c53910ce671a3f313339d86d6dc3
Marina Blin <Bugsyaya@users.noreply.github.com>
2021-08-25 18:28:16 +0200

    :sparkles: Ajout de la modale (#83)

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,170 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Modal as AntdModal } from 'antd';
+import './modal.less'
+import { Button } from '../Button/Button';
+
+/**
+ * 
+ * # 🚧 _Composant en construction_ 🚧
+ * 
+ * # TODO
+ * 
+ * ## Description
+ * 
+ * Documentations andt du composant : https://ant.design/components/modal/
+ * 
+ */
+export const Modal = ({children, ...props }) => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <>
+      {props.OpenModal(showModal)}
+      <AntdModal 
+        className='storybook-modal' 
+        mask={props?.mask}
+        title={props?.title}
+        visible={isModalVisible} 
+        onOk={handleOk} 
+        onCancel={handleCancel}
+        footer={[
+          <Button key="cancel" danger ghost>{props.cancelText}</Button>,
+          <Button key="validate" className="cta">{props.okText}</Button>,
+        ]}
+        {...props}
+      >
+        {children}
+      </AntdModal>
+    </>
+  );
+};
+
+Modal.propTypes = {
+  /**
+  * TODO property
+  */
+  OpenModal: PropTypes.func,
+  /**
+  * TODO property
+  */
+   afterClose: PropTypes.func,
+  /**
+  * TODO property
+  */
+   bodyStyle: PropTypes.string,
+  /**
+  * TODO property
+  */
+   cancelButtonProps: PropTypes.object,
+  /**
+  * TODO property
+  */
+   cancelText: PropTypes.string,
+  /**
+  * TODO property
+  */
+   centered: PropTypes.bool,
+  /**
+  * TODO property
+  */
+   autoFocusButton: PropTypes.oneOfType([null, 'ok', 'cancel']),
+  /**
+  * TODO property
+  */
+   className: PropTypes.string,
+  /**
+  * TODO property
+  */
+   closable: PropTypes.bool,
+  /**
+  * TODO property
+  */
+   closeIcon: PropTypes.node,
+  /**
+  * TODO property
+  */
+   content: PropTypes.node,
+  /**
+  * TODO property
+  */
+   getContainer: PropTypes.node,
+  /**
+  * TODO property
+  */
+  icon: PropTypes.node,
+  /**
+  * TODO property
+  */
+  keyboard: PropTypes.bool,
+  /**
+  * TODO property
+  */
+  mask: PropTypes.bool,
+  /**
+  * TODO property
+  */
+  maskClosable: PropTypes.bool,
+  /**
+  * TODO property
+  */
+  maskStyle: PropTypes.object,
+  /**
+   * TODO property
+  */
+  okButtonProps: PropTypes.node,
+  /**
+  * TODO property
+  */
+  okText: PropTypes.string,
+  /**
+   * TODO property
+  */
+  okType: PropTypes.string,
+  /**
+  * TODO property
+  */
+  style: PropTypes.string,
+  /**
+   * TODO property
+  */
+  title: PropTypes.node,
+  /**
+  * TODO property
+  */
+  width: PropTypes.number,
+  /**
+   * TODO property
+  */
+  zIndex: PropTypes.number,
+  /**
+  * TODO property
+  */
+  onCancel: PropTypes.func,
+  /**
+   * TODO property
+  */
+  onOk: PropTypes.func,
+};
+
+Modal.defaultProps = {
+  OpenModal: (showModal) => <Button type="primary" onClick={showModal}>
+      Ouvrir la modale
+    </Button>,
+  closable: false,
+  mask: false,
+  okText: "Valider",
+  cancelText: "Annuler",
+};
