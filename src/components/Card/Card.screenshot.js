a4dbf51119ff306b3a403166e6395c66ae014803
Marina Blin <Bugsyaya@users.noreply.github.com>
2022-07-10 23:06:43 +0200

    :recycle: Refacto card

--- src/components/Card/Card.screenshot.js
+++ src/components/Card/Card.screenshot.js
@@ -2,27 +2,36 @@ import { ReactScreenshotTest } from 'react-screenshot-test'
 import React from 'react';
 import Card from './Card';
 import 'antd/dist/antd.css';
-import { CloudUploadOutlined, DeleteOutlined, DownloadOutlined, TagsOutlined } from '@ant-design/icons';
+import { CloudUploadOutlined, DeleteOutlined, DownloadOutlined } from '@ant-design/icons';
 import { Tooltip } from '../Tooltip/Tooltip';
+import Tag from '../Tag/Tag';
 
 describe("Screenshot", () => {
   const card = (
     <Card
-      actions={[
-        <Tooltip placement="bottom" title="Télécharger"><DownloadOutlined id="download" /></Tooltip>,
-        <Tooltip placement="bottom" title="Envoyer"><CloudUploadOutlined id="cloud" /></Tooltip>,
-        <Tooltip placement="bottom" title="Supprimer"><DeleteOutlined id="delete" /></Tooltip>
-      ]}
-      cloudTitle="Envoyer"
-      cover={<>
-        <img alt="Etudiant" src="https://avatars.bugsyaya.dev/285/"/>
-        </>}
-      deleteTitle="Supprimer"
-      downloadTitle="Télécharger"
-      icon={<TagsOutlined />}
-      title="Minerva McGonagall"
-      type="lesson"
-    />
+  actions={[
+    <Tooltip placement="bottom" title="Télécharger"><DownloadOutlined id="download" /></Tooltip>,
+    <Tooltip placement="bottom" title="Envoyer"><CloudUploadOutlined id="cloud" /></Tooltip>,
+    <Tooltip placement="bottom" title="Supprimer"><DeleteOutlined id="delete" /></Tooltip>
+  ]}
+  cover={
+    <>
+      <div className="title-card">
+        <div className="container">
+          <Tooltip placement="bottom" title="Métamorphose, Ancient Runes, Arithmancy">
+            <div className="tag-container">
+              <Tag type="lesson">Métamorphose</Tag>
+              <Tag type="lesson">Ancient Runes</Tag>
+              <Tag type="lesson">Arithmancy</Tag>
+            </div>
+          </Tooltip>
+        </div>
+      </div>
+      <img alt="Etudiant" src="https://avatars.bugsyaya.dev/285/"/>
+    </>
+  }
+  title="Minerva McGonagall"
+/>
   );
 
     ReactScreenshotTest
