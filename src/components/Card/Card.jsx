9173a22c7babfec39dfe50cd66b8c724ff3c39a0
Marina Blin <Bugsyaya@users.noreply.github.com>
2022-07-10 23:11:52 +0200

    :recycle: Refacto card (#242)

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,80 +1,95 @@
 import React from 'react';
-import { Card as AntdCard } from 'antd';
+import { Card as AntdCard, Tabs } from 'antd';
 import './card.css'
+import PropTypes from 'prop-types'
 
 /**
  * 
  * Documentations andt du composant : https://ant.design/components/card/
  * 
  */
-export default function Card({children, tags, ...props }){
+export default function Card({children, ...props }){
   return (
     <AntdCard
-      hoverable
       {...props}
     />
   );
 };
 
-// Card.propTypes = {
-//   /**
-//   * TODO property
-//   */
-//    downloadTitle: PropTypes.string,
-//   /**
-//   * TODO property
-//   */
-//    cloudTitle: PropTypes.string,
-//   /**
-//   * TODO property
-//   */
-//    deleteTitle: PropTypes.string,
-//    /**
-//    * TODO property
-//    */
-//     onDownload: PropTypes.func,
-//    /**
-//    * TODO property
-//    */
-//     onCloud: PropTypes.func,
-//    /**
-//    * TODO property
-//    */
-//     onDelete: PropTypes.func,
-//   /**
-//   * TODO property
-//   */
-//    activeTabKey: PropTypes.string,
-//   /**
-//   * TODO property
-//   */
-//    alt: PropTypes.string,
-//   /**
-//   * TODO property
-//   */
-//    src: PropTypes.string,
-//   /**
-//   * TODO property
-//   */
-//    title: PropTypes.node,
-//   /**
-//   * TODO property
-//   */
-//    className: PropTypes.string,
-//   /**
-//   * TODO property
-//   */
-//    tags: PropTypes.array,
-//   /**
-//   * TODO property
-//   */
-//    type: PropTypes.string,
-//   /**
-//   * TODO property
-//   */
-//   icon: PropTypes.node,
-//   /**
-//   * TODO property
-//   */
-//   style: PropTypes.object,
-// };
+Card.propTypes = {
+  /**
+   * La liste d'actions s'affiche en bas de la carte
+   */
+  actions: PropTypes.array,
+  /**
+   * Clé actuelle du TabPane
+   */
+  activeTabKey: PropTypes.string,
+  /**
+   * Style à appliquer au contenu de la carte
+   */
+  bodyStyle: PropTypes.string,
+  /**
+   * Active/désactive le rendu de la bordure autour de la carte
+   */
+  bordered: PropTypes.bool,
+  /**
+   * Couverture de la carte
+  */
+  cover: PropTypes.node,
+  /**
+   * Clé initiale du TabPane actif, si activeTabKey n'est pas défini
+  */
+  defaultActiveTabKey: PropTypes.string,
+  /**
+   * Contenu à afficher dans le coin supérieur droit de la carte
+  */
+  extra: PropTypes.node,
+  /**
+   * Style à appliquer à la tête de carte
+  */
+  headStyle: PropTypes.string,
+  /**
+   * La carte se souleve lorsque vous survolez la carte
+  */
+  hoverable: PropTypes.bool,
+  /**
+  * Affiche un indicateur de chargement pendant que le contenu de la carte est récupéré
+  */
+  loading: PropTypes.bool,
+  /**
+   * Taille de la carte
+  */
+  size: PropTypes.oneOf(['default', 'small']),
+  /**
+  * Contenu supplémentaire dans la barre d'onglets
+  */
+  tabBarExtraContent: PropTypes.node,
+  /**
+   * Liste des têtes de TabPane
+  */
+  tabList: PropTypes.array,
+  /**
+   * CF tabs
+  */
+  tabProps: Tabs,
+  /**
+   * Titre de la carte
+  */
+  title: PropTypes.node,
+  /**
+   * Type de style de carte, peut être défini sur `inner` ou non défini
+  */
+  type: PropTypes.string,
+  /**
+   * Callback lors du changement d'onglet
+  */
+  onTabChange: PropTypes.func,
+};
+
+Card.defaultProps = {
+  bordered: true,
+  hoverable: true,
+  loading: false,
+  size: 'default',
+}
--- src/components/Card/Card.stories.js
+++ src/components/Card/Card.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import results from '../../../.jest-test-results.json';
 import { withTests } from '@storybook/addon-jest';
 import Card from './Card';
-import { CloudUploadOutlined, DeleteOutlined, DownloadOutlined, TagsOutlined } from '../Icon/Icon'
+import { CloudUploadOutlined, DeleteOutlined, DownloadOutlined } from '../Icon/Icon'
 import { Tooltip } from '../Tooltip/Tooltip';
 import Tag from '../Tag/Tag';
 
@@ -47,109 +47,102 @@ Default.args = {
     </div>
     <img src="https://avatars.bugsyaya.dev/285/" alt="Etudiant"/>
   </>,
-  title: "Minerva McGonagall",
-  icon: <TagsOutlined />,
-  type: 'lesson',
-  downloadTitle: 'Télécharger',
-  cloudTitle: 'Envoyer',
-  deleteTitle: 'Supprimer',
+  title: "Minerva McGonagall"
 }
 
 Default.argTypes = {
   actions: {
-    description:"La liste d'actions s'affiche en bas de la carte", 
     control: {
       type: 'object'
     },
   },
-    // count: {
-    //     description:'Numéro à afficher sur le badge', 
-    //     control: {
-    //         type: 'number'
-    //     }
-    // },
-    // dot: {
-    //     description:"Indique s'il faut afficher un point rouge au lieu de compter", 
-    //     control: {
-    //         type: 'boolean'
-    //     },
-    //     table: {
-    //         defaultValue: {
-    //             summary: 'false',
-    //         }
-    //     }
-    // },
-    // offset: {
-    //     description:'Définir le décalage du point du badge (ex : offset: {[10, 10]})', 
-    //     control: {
-    //     type: "array",
-    //     }
-    // },  
-    // overflowCount: {
-    //     description: 'Nombre maximum à afficher',
-    //     control: {
-    //         type: 'number'
-    //     },
-    //     table: {
-    //         defaultValue: {
-    //             summary: '99'
-    //         }
-    //     }
-    // },
-    // showZero: {
-    //     description:"Indique s'il faut afficher le badge lorsque le nombre est égal à zéro", 
-    //     control: {
-    //         type: 'boolean'
-    //     },
-    //     table: {
-    //         defaultValue: {
-    //             summary: 'false'
-    //         }
-    //     }
-    // },
-    // size: {
-    //     description:'Si `count` est défini, `size` définit la taille du badge', 
-    //     control: {
-    //         type: 'select',
-    //         options: [
-    //             'default',
-    //             'small'
-    //         ]
-    //     },
-    //     table: {
-    //         defaultValue: {
-    //             summary: 'default'
-    //         }
-    //     }
-    // },
-    // status: {
-    //     description:'Définir statut du point du badge', 
-    //     control: {
-    //     type: 'select',
-    //     options: [
-    //         'success',
-    //         'processing',
-    //         'default',
-    //         'error',
-    //         'warning'
-    //     ]
-    //     },
-    //     table: {
-    //         defaultValue: {
-    //             summary: 'default'
-    //         }
-    //     }
-    // },
-    // text: {
-    //     description:"Si `status` est défini, `text` définit le texte d'affichage de l'état `dot`", 
-    //     control: {
-    //         type: 'text',
-    //     }
-    // },
-    // title: {
-    //     description:'Texte à afficher au survol du badge', 
-    //     control: {
-    //         type: 'text'
-    //     }
-    // }
+  activeTabKey: {
+    control: {
+      type: 'text'
+    },
+  },
+  bodyStyle: {
+    control: {
+      type: 'text'
+    },
+  },
+  bordered: {
+    control: {
+      type: 'boolean'
+    },
+  },
+  cover: {
+    control: {
+      type: 'object'
+    },
+  },
+  defaultActiveTabKey: {
+    control: {
+      type: 'text'
+    },
+  },
+  extra: {
+    control: {
+      type: 'array'
+    },
+  },
+  headStyle: {
+    control: {
+      type: 'text'
+    },
+  },
+  hoverable: {
+    control: {
+      type: 'boolean'
+    },
+  },
+  loading: {
+    control: {
+      type: 'boolean'
+    },
+  },
+  size: {
+    control: {
+      type: 'text',
+      options: [
+          'default',
+          'small'
+      ]
+    },
+    table: {
+        defaultValue: {
+            summary: 'default'
+        }
+    }
+  },
+  tabBarExtraContent: {
+    control: {
+      type: 'object'
+    },
+  },
+  tabList: {
+    control: {
+      type: 'object'
+    },
+  },
+  tabProps: {
+    control: {
+      type: 'object'
+    },
+  },
+  title: {
+    control: {
+      type: 'object'
+    },
+  },
+  type: {
+    control: {
+      type: 'text'
+    },
+  },
+  onTabChange: {
+    control: {
+      type: 'func'
+    },
+  },
 };
