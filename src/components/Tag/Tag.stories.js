b447163dca8f5de2e4dc64a919c2043739fc1386
Marina Blin <Bugsyaya@users.noreply.github.com>
2022-07-10 19:59:06 +0200

    :recycle: Refacto tag

--- src/components/Tag/Tag.stories.js
+++ src/components/Tag/Tag.stories.js
@@ -1,16 +1,51 @@
 import React from 'react';
-import { Button } from '../Button/Button';
-
-import { Tag } from './Tag';
+import results from '../../../.jest-test-results.json';
+import { withTests } from '@storybook/addon-jest';
+import Tag from './Tag';
 
 export default {
-  title: 'Components/Tag',
   component: Tag,
-  args: {
-    children: "Ananas"
-  }
+  title: 'Atomes/Tag',
+  decorators: [withTests({ results })],
 };
 
-const TagGlobal = (args) => <Tag {...args} />;
+const Template = args => <Tag {...args} />;
 
-export const Defaut = TagGlobal.bind({});
+export const Default = Template.bind({});
+
+Default.args = {
+  children: "Ananas"
+}
+
+Default.argTypes = {
+  closable: {
+        control: {
+            type: 'boolean'
+        },
+    },
+    closeIcon: {
+        control: {
+            type: 'array'
+        }
+    },
+    color: {
+        control: {
+            type: 'color'
+        }
+    },
+    icon: {
+        control: {
+            type: "array",
+        }
+    },  
+    visible: {
+        control: {
+            type: 'boolean'
+        }
+    },
+    onClose: {
+        control: {
+            type: 'boolean'
+        }
+    }
+};
