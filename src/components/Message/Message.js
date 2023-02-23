9c63f5eee9bbeaaae59bb6668f9ca5cf08486459
Marina Blin <Bugsyaya@users.noreply.github.com>
2023-02-23 22:31:10 +0100

    :sparkles: Add message component (#376)

---
+++ src/components/Message/Message.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { message as antdMessage } from 'antd';
+import Button from '../Button/Button';
+import './message.css';
+
+/**
+ * 
+ * # 🚧 _Composant en construction_ 🚧
+ * 
+ * # TODO
+ * 
+ * ## Description
+ * 
+ * Documentations andt du composant : https://ant.design/components/message/
+ * 
+ */
+export const Message = ({ type, content, children, ...props }) => {
+  const [messageApi, contextHolder] = antdMessage.useMessage();
+
+  const onClick = () => {
+    messageApi.open({
+      type,
+      content,
+    });
+  };
+
+  return (
+    <>
+      {contextHolder}
+      <Button
+        className='storybook-message'
+        onClick={onClick}
+        {...props}
+      >
+        {children}
+      </Button>
+    </>
+  );
+};
+
+Message.propTypes = {
+  /**
+   * TODO property
+   */
+  type: PropTypes.string,
+  /**
+   * TODO property
+   */
+  content: PropTypes.string,
+};
+
+Message.defaultProps = {
+};
