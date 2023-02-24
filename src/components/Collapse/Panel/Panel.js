6b0abc9f14706cc2c4c2b8468c276f5033427dc5
Marina Blin <Bugsyaya@users.noreply.github.com>
2023-02-24 11:59:11 +0100

    :sparkles: Add Collapse component

---
+++ src/components/Collapse/Panel/Panel.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Collapse as AntdCollapse } from 'antd';
+import './panel.css';
+
+const { Panel: AntdPanel } = AntdCollapse
+
+/**
+ * 
+ * # 🚧 _Composant en construction_ 🚧
+ * 
+ * # TODO
+ * 
+ * ## Description
+ * 
+ */
+export const Panel = ({ children, ...props }) => {
+  return (
+    <AntdPanel
+      className='storybook-panel'
+      {...props}
+    >
+      {children}
+    </AntdPanel>
+  );
+};
+
+Panel.propTypes = {
+};
+
+Panel.defaultProps = {
+};
