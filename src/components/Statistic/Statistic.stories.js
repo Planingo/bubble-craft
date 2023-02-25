8a220cf471b7c73028a1f9f187b76b33a7872e64
Marina Blin <Bugsyaya@users.noreply.github.com>
2023-02-25 10:14:13 +0100

    :sparkles: Add statistic component

---
+++ src/components/Statistic/Statistic.stories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Statistic } from './Statistic';
+
+export default {
+  title: 'Organismes/Statistic',
+  component: Statistic,
+};
+
+const StatisticGlobal = (args) => <Statistic {...args} />;
+
+export const Default = StatisticGlobal.bind({});
+
+Default.args = {
+    title: "Active users",
+    value: 123456
+};
