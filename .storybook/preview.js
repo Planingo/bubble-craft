fef4e7b5bd194def6242a3f96fcbab3dd580571f
Marina Blin <Bugsyaya@users.noreply.github.com>
2023-03-02 15:41:18 +0100

    ⬆️ Addon (#412)
    * :arrow_up: Addon
    
    * Fix

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,25 +1,9 @@
 import '../Theme/antd.customize.less'
-// import results from '../.jest-test-results.json';
+
 export const parameters = {
     options: {
         storySort: (a, b) =>
         a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
-        isToolshown: true,
     },
 }
-
-export const globalTypes = {
-    locale: {
-      name: 'Locale',
-      description: 'Internationalization locale',
-      defaultValue: 'fr',
-      toolbar: {
-        icon: 'globe',
-        items: [
-          { value: 'en', right: '🇺🇸', title: 'English' },
-          { value: 'fr', right: '🇫🇷', title: 'Français' },
-        ],
-      },
-    },
-  };
   
