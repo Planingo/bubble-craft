8cd9d24212c36f85f4ac11aabc9df1570f9fec49
Bugsyaya <github@bugsyaya.dev>
2023-05-17 21:53:30 +0200

    :arrow_up:Upgrade storybook v6 to v7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,26 +1,16 @@
 module.exports = {
-  'stories': [
-    '../src/Introduction.stories.mdx',
-    "../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"
-  ],
-  'addons': [
-    {
-      name: '@storybook/addon-docs',
-      options: { transcludeMarkdown: true },
-    },
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    'storybook-addon-pseudo-states',
-    '@storybook/addon-measure',
-    '@storybook/addon-jest',
-    {
-      name: '@storybook/addon-styling',
-      options: {
-        postCss: true,
-      },
-    },
-  ],
-  core: {
-    builder: 'webpack5'
+  'stories': ['../src/Introduction.stories.mdx', "../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
+  'addons': [{
+    name: '@storybook/addon-docs',
+    options: {
+      transcludeMarkdown: true
+    }
+  }, '@storybook/addon-links', '@storybook/addon-essentials', 'storybook-addon-pseudo-states', '@storybook/addon-measure', '@storybook/addon-jest', '@storybook/addon-mdx-gfm'],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
   },
-}
+  docs: {
+    autodocs: true
+  }
+};
