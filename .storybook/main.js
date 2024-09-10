120e7b0887bfd2de0bc4e4c2353535cf7c4c5b9a
Valentin Cocaud <v.cocaud@gmail.com>
2024-09-10 21:53:10 +0200

    fix cjs and markdown warning for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,27 +1,48 @@
-module.exports = {
-  'stories': ['../src/Introduction.mdx', "../src/stories/*.@(mdx|stories.@(jsx))"],
-  'addons': [{
-    name: '@storybook/addon-docs',
-    options: {
-      transcludeMarkdown: true
-    }
-  }, '@storybook/addon-links', '@storybook/addon-essentials', 'storybook-addon-pseudo-states', '@storybook/addon-measure', '@storybook/addon-mdx-gfm', '@chromatic-com/storybook'],
+import remarkGfm from "remark-gfm";
+
+export default {
+  stories: [
+    "../src/Introduction.mdx",
+    "../src/stories/*.@(mdx|stories.@(jsx))",
+  ],
+  addons: [
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        transcludeMarkdown: true,
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            remarkPlugins: [remarkGfm],
+          },
+        },
+      },
+    },
+    {
+      name: "@storybook/addon-docs",
+      options: {},
+    },
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "storybook-addon-pseudo-states",
+    "@storybook/addon-measure",
+    "@chromatic-com/storybook",
+  ],
   framework: {
-    name: '@storybook/react-vite',
-    options: {}
+    name: "@storybook/react-vite",
+    options: {},
   },
   core: {
-    builder: '@storybook/builder-vite',
+    builder: "@storybook/builder-vite",
   },
   docs: {},
   async viteFinal(config) {
     // Merge custom configuration into the default config
-    const { mergeConfig } = await import('vite');
+    const { mergeConfig } = await import("vite");
 
     return mergeConfig(config, {
       // Add dependencies to pre-optimization
       optimizeDeps: {
-        include: ['storybook-dark-mode'],
+        include: ["storybook-dark-mode"],
       },
     });
   },
