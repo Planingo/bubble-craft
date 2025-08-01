2f53c6a1879fad6c5ad8c322fdab3750dfddc1be
Bugsyaya <github@bugsyaya.dev>
2025-08-01 20:54:18 +0200

    :sparkles: Intégration du draggable

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -7,6 +7,9 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 
 import { locale } from "../../translation/IntlProvider";
 import "./calendar.css";
+import interactionPlugin, { Draggable } from "@fullcalendar/interaction"
+import { useEffect } from "react";
+import dayjs from "dayjs";
 
 /**
  *
@@ -20,22 +23,44 @@ import "./calendar.css";
  *
  */
 export const Calendar = ({ ...props }) => {
+  console.log("dayjs(Date.now(), YYYY-MM-DD) : ", dayjs(dayjs(), "YYYY-MM-DD"))
+  useEffect(() => {
+    const containerEl = document.querySelector("#events");
+    new Draggable(containerEl, {
+      itemSelector: ".event",
+      eventData: (eventEl) => {
+        return {
+          title: eventEl.innerText
+        };
+      }
+    });
+  }, []);
   return (
     <div className="storybook-calendar">
+
+    <div id="events">
+      {props?.dropEvents?.map(dropEvent => <div key={dropEvent.key} className="event">{dropEvent.title}</div>)}
+    </div>
+    <div className="calendar">
       <FullCalendar
-        schedulerLicenseKey="CC-Attribution-NonCommercial-NoDerivatives"
+        timeZone='UTC'
+        schedulerLicenseKey={props.schedulerLicenseKey}
         headerToolbar={{
           start: "today prev,next",
           center: "title",
           end: "timeGridDay,timeGridWeek,dayGridMonth",
         }}
         locale={locale === "fr" ? fr : en}
-        plugins={[timeGridPlugin, dayGridPlugin, adaptivePlugin]}
-        weekends={true}
+        plugins={[timeGridPlugin, dayGridPlugin, adaptivePlugin, interactionPlugin]}
+        weekends={false}
         initialView="timeGridWeek"
         height="100%"
         events={props.events}
+        editable
+        droppable
+        {...props}
       />
+      </div>
     </div>
   );
 };
