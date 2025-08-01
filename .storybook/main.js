2d1e3491407bd63578b39b4ba5a32a57ae532bd8
Bugsyaya <github@bugsyaya.dev>
2025-08-01 22:35:14 +0200

    Fix lint

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,17 +5,22 @@ export default {
     "../src/Introduction.mdx",
     "../src/stories/*.@(mdx|stories.@(jsx))",
   ],
-  addons: [{
-    name: "@storybook/addon-docs",
-    options: {
-      transcludeMarkdown: true,
-      mdxPluginOptions: {
-        mdxCompileOptions: {
-          remarkPlugins: [remarkGfm],
+  addons: [
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        transcludeMarkdown: true,
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            remarkPlugins: [remarkGfm],
+          },
         },
       },
     },
-  }, "@storybook/addon-links", "storybook-addon-pseudo-states", "@chromatic-com/storybook"],
+    "@storybook/addon-links",
+    "storybook-addon-pseudo-states",
+    "@chromatic-com/storybook",
+  ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
